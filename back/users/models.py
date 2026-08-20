1049d2c42eb18c30bc58978066219804d0741295
Atra <124803075+Atrasoufi@users.noreply.github.com>
2026-08-20 10:41:01 +0330

    feat(users): add phone field to User model

--- back/users/models.py
+++ back/users/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
+    phone = models.CharField(max_length=20, blank=True, default="")
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.email
